fd605abdc59159f0cc3d73b95c132be256ffba9f
JasielAaron <150881134+JasielAaron@users.noreply.github.com>
2024-03-07 15:04:36 -0700

    added code for shapes on shape.js and started code on index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+//file system import
+const fs = require('fs');
+
+//inquirer import
+const inquirer = require('inquirer');
+
+//importing classes from shapes.js
+const { Triangle, Square, Circle} = require('./lib/shapes');
